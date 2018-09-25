4c10dff27f5c649173135ec6f683eda88e10471a
standon19 <42978500+standon19@users.noreply.github.com>
2018-09-25 14:29:17 -0400

    Add files via upload

---
+++ src/TalleyCounter.java
@@ -0,0 +1,53 @@
+
+
+
+
+
+public class TalleyCounter {
+
+	//Instance Variables
+	
+	private int count;
+	
+	
+	//constructor
+	
+	public TalleyCounter();
+	{
+	count = 0;
+	
+	}
+	
+	public TalleyCounter(int startNum)
+	{
+	count = startNum;
+	}
+	
+	public void click()
+	{
+		count = count + 1;
+		//count++
+		//count+=1
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
